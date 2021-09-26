7553e2ba914cd8414bf6b5743468dd14f2ebb502
taka0126ito <taka.major@icloud.com>
2021-09-26 11:44:58 +0900

    ruby バージョンの変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url,  "git@github.com:taka0126ito/original-app.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.0' 
+set :rbenv_ruby, '2.6.5' 
 
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/ti-1016013.pem'] 
